4daace12d1455a4dfa4590b3bbe2ccb7d86c1364
Kasper Hintz <kah@dmi.dk>
2022-01-07 10:08:57 +0100

    Up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='dmit',
-     version='0.5.0',
+     version='0.5.1',
      author="Kasper Hintz",
      author_email="kah@dmi.dk",
      description="A Met Tool package",
